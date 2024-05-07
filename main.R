e296e58ce5f162fe70eed79d71b4ba31f8a5f68b
Carson McKee <k2259011@kcl.ac.uk>
2024-05-07 16:46:35 +0100

    another branch commit

--- main.R
+++ main.R
@@ -1,4 +1,10 @@
 print('Hello GitHub')
 
-samps <- pois(100, 5)
-hist(samps)
+sample_variance <- function(x){
+  n <- length(x)
+  x_bar <- mean(x)
+  sum((x - x_bar)^2)/(n-1)
+}
+
+samps <- rnorm(1000, 0, sqrt(3.14))
+print(sample_variance(samps))
